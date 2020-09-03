2686fd7b9d3b380550750909f4b3676fd244acd2
akizora <cometogetherrightnow1@gmail.com>
2020-09-03 18:29:55 +0900

    fix layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div>
     <top-header/>
     <top-nav/>
+    <new-release/>
     <main id="main" role="main">
     <nuxt/>
     </main>
@@ -12,12 +13,14 @@
 <script>
 import TopHeader from '~/components/TopHeader.vue'
 import TopNav from '~/components/TopNav.vue'
+import NewRelease from '~/components/NewRelease.vue'
 import BottomFooter from '~/components/BottomFooter.vue'
 
 export default {
   components: {
     TopNav,
     TopHeader,
+    NewRelease,
     BottomFooter
   }
 }
