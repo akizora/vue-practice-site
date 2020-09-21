7bcb05289143d2e78e705f14c98a92acb1bc7dca
akizora <cometogetherrightnow1@gmail.com>
2020-09-21 17:12:28 +0900

    fix

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -4,12 +4,13 @@
 				<div id="responsive-nav">
 					<ul class="main-nav nav navbar-nav">
 						<li class="active"><a href="/">Home</a></li>
-						<li><a href="#">Hot Deals</a></li>
-						<li><a href="#">Categories</a></li>
+						<li><a href="#">最近紹介された書籍</a></li>
+						<li><a href="#">多く紹介された書籍</a></li>
+						<!-- <li><a href="#">Categories</a></li>
 						<li><a href="#">Laptops</a></li>
 						<li><a href="#">Smartphones</a></li>
 						<li><a href="#">Cameras</a></li>
-						<li><a href="#">Accessories</a></li>
+						<li><a href="#">Accessories</a></li> -->
 					</ul>
 				</div>
 			</div>
