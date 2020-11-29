0925593cb871b1686b8d187d22fffcf891fc3eaa
akizora <cometogetherrightnow1@gmail.com>
2020-11-29 14:30:37 +0900

    fix result page

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -2,7 +2,6 @@
   <div class="container">
     <div class="border-bottom">検索結果</div>
     <div v-for="book in bookList" :key="book.id" class="row border-bottom">
-      <!-- <div class="d-inline-flex"> -->
       <div class="col-sm-6 p-3 text-center">
         <nuxt-link :to="`/book/${book.id}`">
           <img :src="book.img_url" />
@@ -19,7 +18,6 @@
           </nuxt-link>
         </div>
       </div>
-      <!-- </div> -->
     </div>
   </div>
 </template>
@@ -28,21 +26,37 @@
 import BookApi from "@/plugins/axios/modules/book"
 
 export default {
+  props: ["data"],
   data() {
     return {
       query: "",
       bookList: [],
     }
   },
+  watch: {
+    "$route.query.q": {
+      handler(newVal) {
+        this.query = newVal
+        this.fetchBookList(this.query)
+      },
+      immediate: true,
+    },
+  },
   created() {
+    this.setQuery()
+  },
+  mounted() {
     this.fetchBookList()
   },
   methods: {
-    fetchBookList() {
+    setQuery: function () {
+      this.query = this.$route.query.q || ""
+    },
+    fetchBookList(booklist) {
+      this.bookList = booklist
       BookApi.searchBooks(this.query)
         .then((res) => {
           this.bookList = res.data
-          console.log(this.bookList)
         })
         .catch((err) => {
           console.log(err)
