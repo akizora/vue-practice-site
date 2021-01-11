200595ce33fd5136509356ff5dc1cc874468141f
akizora <cometogetherrightnow1@gmail.com>
2021-01-11 16:52:55 +0900

    fix

--- pages/form/index.vue
+++ pages/form/index.vue
@@ -1,63 +1,52 @@
 <template>
-  <form name="contact" method="POST" netlify>
-    <div class="container mt-3">
-      <div id="form-parts">
-        <div class="row element">
-          <div class="col-md-12 col-xs-12">
-            <div class="panel-group">
-              <label class="txt-larger3 mrt5 must-value">
-                お名前
-                <span class="label_require">*</span>
-              </label>
-              <input
-                name="name"
-                type="text"
-                class="form-control"
-                placeholder="お名前を入力してください"
-                value=""
-              />
-            </div>
-          </div>
-        </div>
-        <div class="row element">
-          <div class="col-md-12 col-xs-12">
-            <div class="panel-group">
-              <label class="txt-larger3 mrt5 must-value">
-                メールアドレス
-                <span class="label_require">*</span>
-              </label>
-              <input
-                name="email"
-                type="email"
-                class="form-control"
-                placeholder="メールアドレスを入力してください"
-                value=""
-              />
-              <p class="control-label" style="display: none" />
-            </div>
-          </div>
-        </div>
-        <div id="fa99581e5e86c0dc9c5fcaa445d0ab48ddae5876" class="row element">
-          <div class="col-md-12 col-xs-12">
-            <div class="panel-group">
-              <label class="txt-larger3 mrt5 must-value">
-                お問い合わせ内容
-                <span class="label_require">*</span>
-              </label>
-              <textarea
-                name="textarea"
-                id="1f4428e12df9de76f8f2065f9f04ec30aa50607e"
-                class="form-control"
-                rows="3"
-                placeholder="お問い合わせ内容を入力してください"
-              />
-              <p class="control-label" style="display: none" />
-            </div>
-          </div>
-        </div>
+  <div class="p-contact">
+    <form
+      class="p-contact__form"
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      @submit.prevent="onSubmit"
+      :class="sendingClass"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <div class="p-contact__item">
+        <label for="username">お名前</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          v-model="username"
+          autocomplete="name"
+        />
       </div>
-    </div>
-  </form>
+      <div class="p-contact__item">
+        <label for="katakana">フリガナ</label>
+        <input type="text" id="katakana" name="katakana" v-model="katakana" />
+      </div>
+      <div class="p-contact__item">
+        <label for="useremail">メールアドレス</label>
+        <input
+          type="text"
+          id="useremail"
+          name="useremail"
+          v-model="useremail"
+          autocomplete="email"
+        />
+      </div>
+      <div class="p-contact__item">
+        <label for="message">お問い合わせ内容</label>
+        <textarea id="message" name="message" v-model="message" />
+      </div>
+      <div class="p-contact__item" v-show="false">
+        <label for="message">スパムでない場合は空欄</label>
+        <input type="text" name="bot-field" v-model="botField" />
+      </div>
+      <div class="p-contact__submit">
+        <button type="submit">送信</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script></script>
