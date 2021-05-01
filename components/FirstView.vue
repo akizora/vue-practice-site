a39335de42965d8fd8237678981fc01e0001feb2
akizora <cometogetherrightnow1@gmail.com>
2021-05-01 14:24:36 +0900

    fix

--- components/FirstView.vue
+++ components/FirstView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid hero-banner">
-    <div class="row h-100 pc">
+    <div class="row h-100">
       <div class="col-6 left-banner d-flex align-items-center">
         <div class="mx-auto">
           <div class="banner">
@@ -19,21 +19,6 @@
         </div>
       </div>
     </div>
-    <div class="row sp">
-      <img src="../assets/img/code2.jpg" alt="New" class="sp-img" />
-    </div>
-    <div class="row sp hero-banner">
-      <div class="col-12 left-banner d-flex align-items-center">
-        <div class="mx-auto">
-          <div class="banner">
-            <h1>Fast look for a tech book.</h1>
-            <p>
-              Qiitaで紹介された書籍を紹介します。<br />毎日ラインナップを更新しています。
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -42,7 +27,7 @@ h1 {
   font-weight: 200;
 }
 .hero-banner {
-  height: 650px;
+  height: 500px;
 }
 .left-banner {
   background: #98dbc6;
@@ -51,26 +36,5 @@ h1 {
   // background: #e6d720;
   background-image: url("../assets/img/code2.jpg");
   background-size: cover;
-}
-.sp {
-  display: none;
-}
-.pc {
-  display: flex;
-}
-@media only screen and (max-width: 767px) {
-  .sp {
-    display: flex;
-  }
-  .pc {
-    display: none;
-  }
-  .sp-img {
-    width: auto;
-    min-height: 490px;
-  }
-  .hero-banner {
-    min-height: 190px;
-  }
 }
 </style>
